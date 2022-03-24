8a4d4439938d5c96353fc750df1e71de4d2ed09c
Mauricio Campos <thoyferr@gmail.com>
2022-03-24 01:47:18 -0300

    finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 const baseURL = 'https://api.github.com/';
 
-
-
 const request = async (direccion) => {
     const resultado = await fetch(`${baseURL}${direccion}`);
     const respuesta = await resultado.json();
@@ -13,61 +11,53 @@ const getUser = async (nombreUsuario) => {
     return request(`users/${nombreUsuario}`);
 }
 
-const getRepo = async (nombreUsuario, numero, pagina) => {
-    return request(`users/${nombreUsuario}/repos?page=${numero}&per_page=${pagina}`);
+const getRepo = async (usuario, numero, pagina) => {
+    return request(`users/${usuario}/repos?page=${numero}&per_page=${pagina}`);
 }
 
 
 const buscar = document.querySelector('#frmformulario');
 buscar.addEventListener('submit', async (event) => {
-    //const arreglousuar = [];
-    //const arreglorepo = [];
     event.preventDefault();
     const nombreUsuario = document.querySelector('#nombre').value;
     const numeroPagina = document.querySelector('#pagina').value;
     const repoPagina = document.querySelector('#repoPagina').value;
-    // const usuario  = await getUser(nombreUsuario);
-    // arreglousuar.push(usuario);
-    // const repositorio = await getRepo(nombreUsuario,numeroPagina,repoPagina);
-    //  arreglorepo.push(repositorio);
+    const falla = `${nombreUsuario} no se reconoce como usuario`;
     if (nombreUsuario === '' || numeroPagina === '' || repoPagina === '') {
         alert(`Debe llenar los campos antes de presionar enviar`);
     } else {
         Promise.all([getUser(nombreUsuario), getRepo(nombreUsuario, numeroPagina, repoPagina)]).then(promesas => {
             let formato = document.querySelector('#resultados');
-            if ((promesas[0].name === null) || (promesas[0].name === undefined)) {
-                throw new Error(`El usuario con nombre ${nombreUsuario}, no se encuentra`);
+            if (promesas[0].name === null) {
+                throw new Error(falla);
             } else {
-                console.log(promesas[0].name);
-                console.log(promesas[1][0].name);
                 formato.innerHTML = `<div class="container col-12 col-sm-10 text-center">
                 <div class="card-deck">
                 <div class="card">
                 <div class="card-header text-white bg-info mb-3 text-center"><h3>Datos Usuario</h3></div>
                 <img src=${promesas[0].avatar_url} class="logo mx-auto">
                   <div class="card-body">
-                    <ul>
+                    <ul class="mx-2">
                         <li class="text-left"><b>Nombre de Usuario : </b>${promesas[0].name}</li>
-                        <li class="text-left">Nombre de Login : ${promesas[0].login}</li>
-                        <li class="text-left">Cantidad de Repositorios : ${promesas[0].public_repos}</li>
-                        <li class="text-left">Localidad : ${promesas[0].location}</li>
-                        <li class="text-left">Tipo de usuario : ${promesas[0].type}</li>
+                        <li class="text-left"><b>Nombre de Login : </b>${promesas[0].login}</li>
+                        <li class="text-left"><b>Cantidad de Repositorios : </b>${promesas[0].public_repos}</li>
+                        <li class="text-left"><b>Localidad : </b>${promesas[0].location}</li>
+                        <li class="text-left"><b>Tipo de usuario : </b>${promesas[0].type}</li>   
                     </ul>
                   </div>
                 </div>
                 <div class="card">
-                    <div class="card-header text-white bg-info mb-3 text-center">Datos Usuario</div>
+                    <div class="card-header text-white bg-info mb-3 text-center"><h3>Nombre Repositorio</h3></div>
                   <div class="card-body">
-                    <ul>
-                        <li>aqui el nombre</li>
-                        <li>aqui el nombre</li>
-                    </ul>
+                    <div id="datRepo" class="text-left">
+                    </div>
                   </div>
                 </div>
-              </div>`
-
+              </div><br> `
+              promesas[1].forEach( (dato,i) => 
+                $('#datRepo').append(`<b>Repositorio N° ${i+1}</b>&nbsp&nbsp<a href="${dato.html_url}" target="_black" > ${dato.name}</a><br>`));
             }
-        });
+        })
+        .catch (fal => alert(falla));
     }
-
 });
